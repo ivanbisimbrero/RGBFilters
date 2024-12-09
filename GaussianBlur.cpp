f8cfbf0eaadd933694d43f77621c861000371a53
Daniel Buxton Sierras <danielbuxtonsierras@Daniels-MacBook-Pro.local>
2024-12-09 10:31:11 +0100

    Add speedup comparison for Gaussian filter

--- GaussianBlur.cpp
+++ GaussianBlur.cpp
@@ -14,9 +14,7 @@ GaussianBlur::GaussianBlur(Image& image) {
     this->pixels = image.pixels;
 }
 
-void GaussianBlur::sequentialFilterCreation(double GKernel[][5]) {
-    // Initialise the standard deviation to 1.0
-    double sigma = 1.0;
+void GaussianBlur::sequentialFilterCreation(double GKernel[][5], double sigma) {
     double s = 2.0 * sigma * sigma;
 
     // Sum is for normalization 
@@ -40,19 +38,13 @@ void GaussianBlur::sequentialFilterCreation(double GKernel[][5]) {
     }
 }
 
-vector<vector<Pixel> > GaussianBlur::sequentialConvolution(double GKernel[][5], int iterations) {
-    vector<vector<Pixel> > new_pixels(height, vector<Pixel>(width));
+vector<vector<Pixel>> GaussianBlur::sequentialConvolution(double GKernel[][5], int iterations) {
+    vector<vector<Pixel>> source = pixels; // Use pixels as the initial source
+    vector<vector<Pixel>> destination(height, vector<Pixel>(width));
+
     int i, j, m, n, mm, nn;
     double val_r, val_g, val_b;
 
-    // Copy the pixels to the new_pixels matrix
-    for (i = 0; i < height; ++i) {
-        for (j = 0; j < width; ++j) {
-            new_pixels[i][j] = pixels[i][j];
-        }
-    }
-
-    // Iterate over the number of iterations
     for (int it = 0; it < iterations; ++it) {
         // Iterate over the image
         for (i = 0; i < height; ++i) {
@@ -60,32 +52,34 @@ vector<vector<Pixel> > GaussianBlur::sequentialConvolution(double GKernel[][5], 
                 val_r = 0.0;
                 val_g = 0.0;
                 val_b = 0.0;
-                // Iterate over the Gaussian kernel
+
+                // Apply the Gaussian kernel
                 for (m = -2; m <= 2; ++m) {
                     mm = i + m;
                     for (n = -2; n <= 2; ++n) {
                         nn = j + n;
                         if (mm >= 0 && mm < height && nn >= 0 && nn < width) {
-                            val_r += new_pixels[mm][nn].r * GKernel[m + 2][n + 2];
-                            val_g += new_pixels[mm][nn].g * GKernel[m + 2][n + 2];
-                            val_b += new_pixels[mm][nn].b * GKernel[m + 2][n + 2];
+                            val_r += source[mm][nn].r * GKernel[m + 2][n + 2];
+                            val_g += source[mm][nn].g * GKernel[m + 2][n + 2];
+                            val_b += source[mm][nn].b * GKernel[m + 2][n + 2];
                         }
                     }
                 }
-                new_pixels[i][j].r = val_r;
-                new_pixels[i][j].g = val_g;
-                new_pixels[i][j].b = val_b;
+                destination[i][j].r = val_r;
+                destination[i][j].g = val_g;
+                destination[i][j].b = val_b;
             }
         }
-        pixels = new_pixels;
+
+        // Swap the source and destination buffers
+        std::swap(source, destination);
     }
 
-    return new_pixels;
+    pixels = source; // Assign the final result back to pixels
+    return pixels;   // Return the blurred image
 }
 
-void GaussianBlur::parallelFilterCreation(double GKernel[][5]) {
-    // Initialise the standard deviation to 1.0
-    double sigma = 1.0;
+void GaussianBlur::parallelFilterCreation(double GKernel[][5], double sigma) {
     double s = 2.0 * sigma * sigma;
 
     // Sum is for normalization 
@@ -112,46 +106,43 @@ void GaussianBlur::parallelFilterCreation(double GKernel[][5]) {
 }
 
 vector<vector<Pixel> > GaussianBlur::parallelConvolution(double GKernel[][5], int iterations) {
-    vector<vector<Pixel> > new_pixels(height, vector<Pixel>(width));
+    vector<vector<Pixel>> source = pixels; // Use pixels as the initial source
+    vector<vector<Pixel>> destination(height, vector<Pixel>(width));
+
     int i, j, m, n, mm, nn;
     double val_r, val_g, val_b;
 
-    // Copy the pixels to the new_pixels matrix
-    #pragma omp parallel for
-    for (i = 0; i < height; ++i) {
-        for (j = 0; j < width; ++j) {
-            new_pixels[i][j] = pixels[i][j];
-        }
-    }
-
-    // Iterate over the number of iterations
     for (int it = 0; it < iterations; ++it) {
         // Iterate over the image
-        #pragma omp parallel for private(val_r, val_g, val_b, m, n, mm, nn)
+        #pragma omp parallel for collapse(2) private(val_r, val_g, val_b, m, n, mm, nn)
         for (i = 0; i < height; ++i) {
             for (j = 0; j < width; ++j) {
                 val_r = 0.0;
                 val_g = 0.0;
                 val_b = 0.0;
-                // Iterate over the Gaussian kernel
+
+                // Apply the Gaussian kernel
                 for (m = -2; m <= 2; ++m) {
                     mm = i + m;
                     for (n = -2; n <= 2; ++n) {
                         nn = j + n;
                         if (mm >= 0 && mm < height && nn >= 0 && nn < width) {
-                            val_r += new_pixels[mm][nn].r * GKernel[m + 2][n + 2];
-                            val_g += new_pixels[mm][nn].g * GKernel[m + 2][n + 2];
-                            val_b += new_pixels[mm][nn].b * GKernel[m + 2][n + 2];
+                            val_r += source[mm][nn].r * GKernel[m + 2][n + 2];
+                            val_g += source[mm][nn].g * GKernel[m + 2][n + 2];
+                            val_b += source[mm][nn].b * GKernel[m + 2][n + 2];
                         }
                     }
                 }
-                new_pixels[i][j].r = val_r;
-                new_pixels[i][j].g = val_g;
-                new_pixels[i][j].b = val_b;
+                destination[i][j].r = val_r;
+                destination[i][j].g = val_g;
+                destination[i][j].b = val_b;
             }
         }
-        swap(pixels, new_pixels);
+
+        // Swap the source and destination buffers
+        std::swap(source, destination);
     }
 
-    return new_pixels;
+    pixels = source; // Assign the final result back to pixels
+    return pixels;   // Return the blurred image
 }
--- GaussianBlur.h
+++ GaussianBlur.h
@@ -11,10 +11,10 @@ class GaussianBlur {
         GaussianBlur();
         GaussianBlur(Image& image);
         // Sequential methods
-        void sequentialFilterCreation(double GKernel[][5]);
+        void sequentialFilterCreation(double GKernel[][5], double sigma);
         vector<vector<Pixel> > sequentialConvolution(double GKernel[][5], int iterations);
         // Parallelised methods
-        void parallelFilterCreation(double GKernel[][5]);
+        void parallelFilterCreation(double GKernel[][5], double sigma);
         vector<vector<Pixel> > parallelConvolution(double GKernel[][5], int iterations);
     private:
         vector<vector<Pixel> > pixels;
